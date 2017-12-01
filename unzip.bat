7ecf5848dc47a46eeb627303a11aa754b778fd34
sunsnowswift <sunsnowswift@gmail.com>
2017-12-01 08:36:36 +0800

    Update unzip.bat

--- unzip.bat
+++ unzip.bat
@@ -1,5 +1,4 @@
 ::循环脚本所在目录的所有一级子目录并将每个子目录的所有zip文件解压缩并删除
-::guoqiang 20171130
 
 @echo off
 ::请先安装7-zip,设置7-zip的运行路径
@@ -11,7 +10,7 @@ for /d %%i in (*) do (
 	cd /d "%srcdir%\%%i"
 	::循环目录下的zip文件并解压,解压后删除zip文件
 	for /f %%j in ('dir *.zip /s /b') do (
-		::解压
+	::解压
     	start "" "%zpath%" x "%%j" -y -aos -o"%%~dpi"
     	::删除
     	del "%%j"
